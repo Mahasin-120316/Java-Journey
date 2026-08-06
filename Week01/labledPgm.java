6aa4f324af97143752a43f642dcbdad2191e6630
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-08-06 13:10:35 +0530

    labledPgm

--- Week01/labledPgm.java
+++ Week01/labledPgm.java
@@ -1,11 +1,13 @@
 
-public class NestedPgm2 {
+public class labledPgm {
 
     public static void main(String[] args) {
+        outer:
         for (int i = 1; i <= 3; i++) {
-            for (int j = 1; j <= 2; j++) {
+            inner:
+            for (int j = 1; j <= 4; j++) {
                 if (j == 2) {
-                    continue;
+                    break outer;
                 }
                 System.out.println("i:" + i + "j:" + j);
             }
