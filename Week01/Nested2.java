d8377b82e37d799bfb313c69777661ae9c956f4d
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-08-06 12:31:18 +0530

    Nested while

---
+++ Week01/Nested2.java
@@ -0,0 +1,14 @@
+
+public class Nested2 {
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 1;
+        while (i <= 5) {
+            while (j <= 5) {
+                System.out.println(j);
+                j++;
+            }
+            i++;
+        }
+    }
+}
