9cf8bf7abf90133b7da12cc33d8736f1db67b3c9
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-08-17 12:50:55 +0530

    Array2

---
+++ Week01/Week02/Pgm2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Pgm2 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        int a[] = new int[5];
+        System.out.println("Enter Array Elements: ");
+        for(int i = a.length-1;i >= 0;i--){
+            a[i] = sc.nextInt();
+        }
+        System.out.println("Array Elements: ");
+        for(int i = a.length-1;i >= 0;i--){
+            System.out.println(a[i]);
+        }
+        sc.close();
+    
+         
+    }
+}
