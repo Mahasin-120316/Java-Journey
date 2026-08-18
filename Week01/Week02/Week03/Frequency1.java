4fefecc814794871f289f05d38a580f6b159bcc4
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-08-18 20:52:23 +0530

    Frequency1

---
+++ Week01/Week02/Week03/Frequency1.java
@@ -0,0 +1,30 @@
+package Week03;
+import java.util.Scanner;
+
+public class Frequency1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+        int count = 0;
+
+        // One loop and one equality check as per constraint
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == target) {
+                count++;
+            }
+        }
+
+        System.out.println("Frequency of " + target + ": " + count);
+
+        sc.close();
+    }
+}
+
