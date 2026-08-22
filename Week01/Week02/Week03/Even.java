284de7220bee1e833eed344fceeeae9684cba022
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-08-22 19:39:17 +0530

    Even

--- Week01/Week02/Week03/Even.java
+++ Week01/Week02/Week03/Even.java
@@ -1,16 +1,16 @@
-5
 import java.util.Scanner;
-
-public class Num1 {
+public class Even {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter N: ");
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            System.out.print(i + " ");
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
         }
-        sc.close();
+    sc.close();
     }
 }
+
