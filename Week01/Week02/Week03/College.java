62fc943e9bfbd27cc809b423f189d015a52da3af
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-08-23 21:35:40 +0530

    College1

--- Week01/Week02/Week03/College.java
+++ Week01/Week02/Week03/College.java
@@ -4,15 +4,17 @@ class College {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
-        int departments = 2;
-        int classes = 2;
-        int students = 3;
+        System.out.println("Enter the number of departments: ");
+        int departments = sc.nextInt();
+        System.out.println("Enter the number of classes: ");
+        int classes = sc.nextInt();
+        System.out.println("Enter the number of students: ");
+        int students = sc.nextInt();
 
         int[][][] marks = new int[departments][classes][students];
 
         int total = 0;
-
+        System.out.println("Enter the marks: ");
         // Reading marks
         for (int i = 0; i < departments; i++) {
             for (int j = 0; j < classes; j++) {
