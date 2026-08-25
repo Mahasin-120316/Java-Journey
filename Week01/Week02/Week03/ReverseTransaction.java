2f6a055eb77a32ac7fb8c2734e21ee13c756a882
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-08-25 12:02:13 +0530

    ReverseTransaction

---
+++ Week01/Week02/Week03/ReverseTransaction.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ReverseTransaction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] transaction = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            transaction[i] = sc.nextInt();
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(transaction[i] + " ");
+        }
+        sc.close();
+    }
+}
