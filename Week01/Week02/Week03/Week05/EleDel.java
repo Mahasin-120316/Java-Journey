081ab410ec5aa179200816fb9ba467b3807096f8
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-09-01 14:51:48 +0530

    EleDelete

---
+++ Week01/Week02/Week03/Week05/EleDel.java
@@ -0,0 +1,21 @@
+package Week05;
+
+public class EleDel {
+    public static void main(String[] args){
+        int [] arr = {10,20,30,40,50};
+        int [] arr2 = new int[4];
+        int element = 30;
+        int index = 2;
+        for(int i=0;i<arr2.length;i++){
+            if(i<index){
+                arr2[i] = arr[i];
+            }
+            else{
+                arr2[i] = arr[i+1];
+            }
+        }
+        for(int i=0;i<arr2.length;i++){
+            System.out.println(arr2[i]);
+        }
+    }
+}
