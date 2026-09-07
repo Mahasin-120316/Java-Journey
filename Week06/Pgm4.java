a170dbcb2e09097f2f0a09690650d2d2824bc0de
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-09-07 17:27:14 +0530

    Pgm4 of understanding Strings

---
+++ Week06/Pgm4.java
@@ -0,0 +1,13 @@
+package Week06;
+
+public class Pgm4 {
+    public static void main(String[] args){
+        String s1 = new String("Hello2");
+        String s2 = new String("Hello2");
+        if(s1.equals(s2)){
+            System.out.println("Strings are same");
+        }else{
+            System.out.println("Strings are not same");
+        }
+    }
+}
