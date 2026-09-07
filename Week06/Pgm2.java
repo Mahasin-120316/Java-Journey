bd56e5c47fdeac8c93c21392902b33605ca4ba53
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-09-07 17:21:35 +0530

    Pgm2 of understanding Strings

---
+++ Week06/Pgm2.java
@@ -0,0 +1,13 @@
+package Week06;
+
+public class Pgm2 {
+    public static void main(String[] args){
+        String s1 = new String("Hello");
+        String s2 = new String("Hello");
+        if(s1 == s2){
+            System.out.println("Equal");
+        }else{
+            System.out.println("Not Equal");
+        }
+    }
+}
