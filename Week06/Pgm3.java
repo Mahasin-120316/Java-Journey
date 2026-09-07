5c26f1912f107d1cc22f1bf178f8df3f3759be89
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-09-07 17:24:03 +0530

    Pgm3 of understanding Strings

---
+++ Week06/Pgm3.java
@@ -0,0 +1,13 @@
+package Week06;
+
+public class Pgm3 {
+    public static void main(String[] args){
+        String s1 = "Hello2";
+        String s2 = "Hello";
+        if(s1.equals(s2)){
+            System.out.println("Strings are same");
+        }else{
+            System.out.println("Strings are not same");
+        }
+    }
+}
