b6ef443d00ca24215403ca54b5dbb939e5545625
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-09-07 21:10:58 +0530

    Missing numbers

---
+++ Week06/MissingNum.java
@@ -0,0 +1,16 @@
+package Week06;
+import java. util.Scanner;
+public class MissingNum {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    long n = sc.nextLong();
+    long expectedSum = n * (n + 1) / 2;
+    long actualSum = 0;
+    for(int i = 0;i < n - 1;i++){
+        actualSum += sc.nextLong();
+    }
+    long missingNumber = expectedSum - actualSum;
+    System.out.println(missingNumber);
+    sc.close();
+}
+}
