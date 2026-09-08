fbc671b367f057fc22432a5771ae3065b3d1d5ad
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-09-08 16:53:32 +0530

    Equal ignore case

---
+++ Week06/Pgm5.java
@@ -0,0 +1,13 @@
+package Week06;
+
+public class Pgm5 {
+    public static void main(String[] args){
+        String s1 = "java";
+        String s2 = "Java";
+        if(s1.equalsIgnoreCase(s2)){
+            System.out.println(" Strings are equal");
+        }else{
+            System.out.println(" Strings are not equal");
+        }
+    }
+}
