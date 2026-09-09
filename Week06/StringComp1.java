533f3f8624bf87b6bea558dae2f31b901e276d74
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-09-09 16:56:28 +0530

    Comparing Stirngs1

---
+++ Week06/StringComp1.java
@@ -0,0 +1,15 @@
+package Week06;
+
+public class StringComp1 {
+    public static void main(String[] args){
+        String s1 = "Mahasin";
+        String s2 = "Humaira";
+        String s3 = s1 + s2;
+        System.out.println(s3);
+        String str1 = new String("Dustakar");
+        String str2 = new String("Tulsi");
+        String res = str1.concat(str2);
+        System.out.println(res);
+        
+    }
+}
