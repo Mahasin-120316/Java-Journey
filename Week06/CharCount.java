ed4939560f0645643ee9e4c057f586019c01fb93
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-09-10 12:10:43 +0530

    counting the characters of a string

---
+++ Week06/CharCount.java
@@ -0,0 +1,18 @@
+package Week06;
+import java.util.Scanner;
+
+public class CharCount{
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String text = scanner.nextLine();
+    char target = scanner.nextLine().charAt(0);
+    int count = 0;
+    for(int i = 0;i < text. length(); i++){
+        if(text.charAt(i) == target){
+            count++;
+        }
+    }
+        System.out.println("Occurrences: " + count);
+        scanner.close();
+}
+}
