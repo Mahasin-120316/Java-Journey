5046b38da6ed23f94a9a3992fe4f9b087ac7864b
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-09-10 17:48:25 +0530

    StringBuilder2

---
+++ Week06/RevStr.java
@@ -0,0 +1,20 @@
+package Week06;
+import java.util.Scanner;
+public class RevStr {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String");
+        String str = sc.next();
+        char arr[] = str.toCharArray();
+        char newArr[] = new char[arr.length];
+        int j = newArr.length-1;
+        for(int i = 0; i < arr.length; i++){
+            newArr[j] = arr[i];
+            j--;
+        }
+        String revStr = new String(newArr);
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + revStr);
+        sc.close();
+    }
+}
