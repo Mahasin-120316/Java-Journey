f701fb84a730fc5250a92c2c20cc9f47d038470f
Mahasin Humaira <mahasinshaik2005@gmail.com>
2026-09-25 12:56:22 +0530

    Even and Odd

---
+++ Week07/EvenOdd.java
@@ -0,0 +1,20 @@
+package Week07;
+import java.util.Scanner;
+
+public class EvenOdd {
+
+public static void main (String[] args)
+{
+Scanner scan = new Scanner(System.in);
+int n=scan.nextInt();
+
+if (n % 2 == 0)
+{
+    System.out.println("even");
+}
+else{
+    System.out.println("odd");
+}
+scan.close();
+}
+}
